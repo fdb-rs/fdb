a4874855002af74ea547e0eb385b0bcce1b54e4e
Rajiv Ranganath <rajiv.ranganath@atihita.com>
2022-05-12 09:29:46 +0530

    docs: add minimal example
    This was suggested by Fredrik Jansson so users who visit the docs.rs
    site can see a minimal example.
    
    Co-authored-by: Fredrik Jansson <fj@avassa.io>

--- fdb/src/lib.rs
+++ fdb/src/lib.rs
@@ -11,6 +11,46 @@
 //! level documentation here.
 //!
 //! [website]: https://fdb-rs.github.io/docs/getting-started/introduction/
+//!
+//! A minimal example to get started.
+//!
+//! ```no_run
+//! use tokio::runtime::Runtime;
+//!
+//! use std::env;
+//! use std::error::Error
+//!
+//! fn main() -> Result<(), Box<dyn Error>> {
+//!     let fdb_cluster_file = env::var("FDB_CLUSTER_FILE").expect("FDB_CLUSTER_FILE not defined!");
+//!
+//!     unsafe {
+//!         fdb::select_api_version(710);
+//!         fdb::start_network();
+//!     }
+//!
+//!     let fdb_database = fdb::open_database(fdb_cluster_file)?;
+//!
+//!     let rt = Runtime::new()?;
+//!
+//!     let cloned_fdb_database = fdb_database.clone();
+//!
+//!     rt.block_on(async {
+//!         let fdb_database = cloned_fdb_database;
+//!
+//!         // your main async app here
+//!
+//!         Result::<(), Box<dyn Error>>::Ok(())
+//!     })?;
+//!
+//!     drop(fdb_database);
+//!
+//!     unsafe {
+//!         fdb::stop_network();
+//!     }
+//!
+//!     Ok(())
+//! }
+//! ```
 
 mod fdb;
 mod key_value;
